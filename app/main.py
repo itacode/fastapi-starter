907e774c83f72d777783bdb4bde87bb82157e59e
itacode <9909272+itacode@users.noreply.github.com>
2023-03-29 12:45:09 +0900

    tweak cors

--- app/main.py
+++ app/main.py
@@ -7,7 +7,14 @@ from starlette.middleware.cors import CORSMiddleware
 from app.api.router import api_router
 
 app = FastAPI()
-app.add_middleware(CORSMiddleware)
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+)
+
 app.include_router(api_router, prefix="/api/v1/my_service")
 
 # https://stackoverflow.com/questions/7507825/where-is-a-complete-example-of-logging-config-dictconfig
