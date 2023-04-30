a24bdebdbb0241a3e1647b45fb4ec0186218acc6
itacode <9909272+itacode@users.noreply.github.com>
2023-04-30 09:26:59 +0900

    update deps

--- app/entities/orm/orm_model.py
+++ app/entities/orm/orm_model.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from datetime import datetime
 
-from sqlalchemy import ForeignKey, String, text
+from sqlalchemy import ForeignKey, text
 from sqlalchemy.dialects.mysql import DATETIME
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
@@ -14,14 +14,12 @@ class User(Base):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True, unique=True)
-    username: Mapped[str] = mapped_column(String(16), nullable=False)
-    email: Mapped[str] = mapped_column(nullable=False)
-    is_deleted: Mapped[bool] = mapped_column(nullable=False, server_default=text("'0'"))
-    create_time: Mapped[datetime] = mapped_column(
-        DATETIME(fsp=6), nullable=False, server_default=text("CURRENT_TIMESTAMP(6)")
-    )
+    username: Mapped[str]
+    email: Mapped[str]
+    is_deleted: Mapped[bool] = mapped_column(server_default=text("'0'"))
+    create_time: Mapped[datetime] = mapped_column(DATETIME(fsp=6), server_default=text("CURRENT_TIMESTAMP(6)"))
     update_time: Mapped[datetime] = mapped_column(
-        DATETIME(fsp=6), nullable=False, server_default=text("CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)")
+        DATETIME(fsp=6), server_default=text("CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)")
     )
 
     posts: Mapped[list["Post"]] = relationship(back_populates="user")
@@ -31,14 +29,12 @@ class Post(Base):
     __tablename__ = "post"
 
     id: Mapped[int] = mapped_column(primary_key=True, unique=True)
-    content: Mapped[str] = mapped_column(nullable=False)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False, index=True)
-    is_deleted: Mapped[bool] = mapped_column(nullable=False, server_default=text("'0'"))
-    create_time: Mapped[datetime] = mapped_column(
-        DATETIME(fsp=6), nullable=False, server_default=text("CURRENT_TIMESTAMP(6)")
-    )
+    content: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), index=True)
+    is_deleted: Mapped[bool] = mapped_column(server_default=text("'0'"))
+    create_time: Mapped[datetime] = mapped_column(DATETIME(fsp=6), server_default=text("CURRENT_TIMESTAMP(6)"))
     update_time: Mapped[datetime] = mapped_column(
-        DATETIME(fsp=6), nullable=False, server_default=text("CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)")
+        DATETIME(fsp=6), server_default=text("CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)")
     )
 
     user: Mapped["User"] = relationship(back_populates="posts")
